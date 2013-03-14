f38e505c9ded16d5466d11212cc90583e60c2b99
U-KOMAL\user <user@komal.(none)>
2013-03-15 02:41:38 +0530

    commit app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeleAccount::Application.config.secret_token = 'bf6a935042a2bc004a25b780560088ea0db65eee581c00ebb57c1984e52c73456e83a2af507b80483524b2c8f0b6b82c72bf0b693eb9c2d968a39d0df3e6e453'
