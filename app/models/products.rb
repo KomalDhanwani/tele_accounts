694859fdece5cc9f70806c170cfb54c9f2ca8983
Komal Dhanwani <komal@complitech.net>
2013-04-16 19:36:28 +0530

    remove old one

--- app/models/products.rb
+++
@@ -1,4 +0,0 @@
-class Products < ActiveRecord::Base
-  attr_accessible :name, :price, :selling_price, :quantity, :stocked_on, :photo
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-end
